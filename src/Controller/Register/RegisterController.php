a1648e2c75e537fab8d03d91afd10e88fba87ab8
Bartlomiej Szyszkowski <bartlomiej.szyszkowski.worker@yellows.eu>
2020-08-10 09:51:17 +0200

    staged files

--- src/Controller/Register/RegisterController.php
+++ src/Controller/Register/RegisterController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller\Register;
 
+use App\Entity\User\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +15,11 @@ class RegisterController extends AbstractController
      * @Route("/", name="homepage")
      */
     public function Homepage(): Response{
-        return $this->render('homepage.html.twig');
+        $user = $this->getUser();
+        if ($user == null){
+            return $this->render('Homepage/registerType.html.twig');
+        } else {
+            return $this->render('homepage.html.twig');
+        }
     }
 }
