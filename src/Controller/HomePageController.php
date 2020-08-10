5ffb5b6ca036a77207ff65fc48ece3ed49f33ae7
MrPogos <pogo012356@gmail.com>
2020-08-10 09:43:41 +0200

    create user usecase

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomePageController extends AbstractController
+{    /**
+ * @return Response
+ * @Route("/", name="homepage")
+ */
+    public function main(): Response{
+        return $this->redirectToRoute('locales');    }
+
+
+
+    /**
+     * @return Response
+     * @Route("/{_locale}/", name="locales", defaults={"_locale":"%locale%"}, requirements={"_locale":"%app_locales%"})
+     */
+    public function Homepage(): Response{
+        return $this->render('homepage.html.twig');
+    }
+
+
+}
