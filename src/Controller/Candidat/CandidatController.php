1ececa624564f584d3a010bc30fa65adaa76823d
MrPogos <pogo012356@gmail.com>
2020-08-12 16:18:19 +0200

    end candidatedit

--- src/Controller/Candidat/CandidatController.php
+++ src/Controller/Candidat/CandidatController.php
@@ -4,7 +4,9 @@
 namespace App\Controller\Candidat;
 
 
+use App\Entity\CandidateProfil\UseCase\ChangePhoto;
 use App\Entity\CandidateProfil\UseCase\EditCandidate;
+use App\Entity\User\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -30,22 +32,28 @@ class CandidatController extends AbstractController
     /**
      * @Route("/{_locale}/Candidat/addCandidat", name="addCandidat")
      */
-    public function addCandidat(Request $request,EditCandidate $editCandidate){
+    public function addCandidat(Request $request,EditCandidate $editCandidate,ChangePhoto $photo){
 
 
         $formData =$request->request->all();
         $command= new EditCandidate\Command($this->getUser()->getCandidateProfil(),$formData['growth'],$formData['physique'],$formData['HairLength'],$formData['HairColor'],$formData['EyeColor'],$formData['age']);
         $editCandidate->execute($command);
-        echo "<pre>";
-var_dump($request);die;
-if ($request->files->get('post')['file']!=null) {
-            $file = $request->files->get('post')['my_file'];
+
+        if ($request->files->get('file')!=null) {
+
+            $file = $request->files->get('file');
             $uploads_directory = $this->getParameter('uploads_directory');
             $filename = md5(uniqid()) . '.' . $file->guessExtension();
-            dd($file);
             $file->move(
                 $uploads_directory, $filename
             );
+            $scrPhoto=$uploads_directory."/".$filename;
+
+            /** @var User $user */
+            $user=$this->getUser();
+            $command=new ChangePhoto\Command($user,$scrPhoto);
+            $photo->execute($command);
+
         }
         return $this->redirectToRoute('homepage');
 
