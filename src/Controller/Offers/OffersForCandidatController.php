0a77d846755b3a335b25d24516e44538ab39e509
MrPogos <pogo012356@gmail.com>
2020-08-25 12:10:07 +0200

    query

--- src/Controller/Offers/OffersForCandidatController.php
+++ src/Controller/Offers/OffersForCandidatController.php
@@ -4,6 +4,8 @@
 namespace App\Controller\Offers;
 
 
+use App\Adapter\Offers\OffersQuery;
+use App\Entity\User\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,8 +15,12 @@ class OffersForCandidatController extends AbstractController
     /**
      * @Route("/{_locale}/Candidat/show/offer")
      */
-    public function main()
+    public function main(OffersQuery $query)
     {
-
+        /** @var User $user */
+        $user=$this->getUser();
+        $Candidate=$user->getCandidateProfil();
+dd($query->findForCandidat($Candidate->getPhysique(),$Candidate->getHairLength(),$Candidate->getHairColor(),$Candidate->getEyeColor(),
+$Candidate->getSex(),$Candidate->getGrowth(),$Candidate->getAge()));
     }
 }
--- src/Entity/Offers/Offers/ReadModel/OffersQueryInterface.php
+++ src/Entity/Offers/Offers/ReadModel/OffersQueryInterface.php
@@ -7,4 +7,5 @@ namespace App\Entity\Offers\Offers\ReadModel;
 interface OffersQueryInterface
 {
 public function findByUser(int $userid);
+public function findForCandidat(string $physique,string $hairLength,string $hairColor,string $eyeColor, string $Sex,int $Growth,int $Age);
 }
